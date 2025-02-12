63d0cd301475c50590e46f10eda58674530d7365
ANOO-DESKTOP\Anoo <info@anoo.nl>
2025-02-12 18:55:55 +0100

    chore: Update Netlify and Next.js configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  distDir: '.next',
   compiler: {
     styledComponents: true,
   },
