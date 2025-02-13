b24e2166145c42dccb7c77e3846aff5104060856
ANOO-DESKTOP\Anoo <info@anoo.nl>
2025-02-13 20:59:14 +0100

    fix: revert content spacing

--- src/components/layout/styles.ts
+++ src/components/layout/styles.ts
@@ -2,8 +2,5 @@ import styled from 'styled-components';
 
 export const MainContent = styled.div`
   position: relative;
-`;
-
-export const Spacer = styled.div`
-  height: 64px; /* Provides space for absolute-positioned navbar elements */
-`;
+  padding-top: 64px; /* Navbar height */
+`; 
